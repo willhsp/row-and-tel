b092a5d0cb095b9b10c5ada06b9f8efdb76021c6
Will Powell <William.Powell@softwire.com>
2022-01-25 21:52:42 +0000

    generate project files

---
+++ app/src/main/java/com/willhsp/row_and_tel/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.willhsp.row_and_tel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
